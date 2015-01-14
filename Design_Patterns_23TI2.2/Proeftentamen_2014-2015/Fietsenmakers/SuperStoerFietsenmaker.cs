ee2836dfe0087369b5096ce661c5f1d6f4a7ca80
rangemaster <rangemaster_r@hotmail.com>
2015-01-14 13:34:12 +0100

    Setup Proeftentamen
    Added all classes

---
+++ Design_Patterns_23TI2.2/Proeftentamen_2014-2015/Fietsenmakers/SuperStoerFietsenmaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proeftentamen_2014_2015.Fietsenmakers
+{
+    public class SuperStoerFietsenmaker : Fietsenmaker
+    {
+    }
+}
